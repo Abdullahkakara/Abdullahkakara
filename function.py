45ef3b769e176dc81819ee6b4a60dc38ab0ebb7e
Abdullahkakara <mamunlotani@gmail.com>
2023-11-24 00:29:50 +0600

    added function.py

--- function.py
+++ function.py
@@ -1,4 +1,10 @@
 
+def my_function(fname):
+    print(fname + " True")
+
+my_function("Email")
+my_function("Tobis")
+my_function("Linus")
 
 
 
--- list.py
+++ list.py
@@ -5,7 +5,7 @@
 
 list_1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-list_2 = ["abcd","efgh"]
+list_2 = ["abcdoefgh"]
 sum = 0
 for i in list_2:
     sum = sum+len(i)
