b799ee80b2bcc29a3919424795399727ad7e1e45
Abdullahkakara <mamunlotani@gmail.com>
2023-11-25 06:47:46 +0600

    added loop.py

--- loop.py
+++ loop.py
@@ -29,5 +29,58 @@
 # sum = (ord("s") + (ord("a") + (ord("l") + (ord("m") + (ord("a") + (ord("n")))))))
 # print(sum)
 
-for i in "apple":
-    print(i)
+# for i in "apple":
+#     print(i)
+
+
+# while loop statement:
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i = i+2
+
+# print("end the test")
+
+# i = 1 
+# while i < 6:
+#     print(i)
+#     i = i + 1 
+# else:
+#     print("i is no longer less than 6")
+
+# For loop statement:
+# for x in "banana":
+#     print(x)
+
+# fruits_name = ["apple", "banana","coconut"]
+# for x in fruits_name:
+#     print(x)
+#     if x == "banana":
+#         break
+
+# loop continue statement:
+# fruits = ["apple","banana","cherry", "goava", "coconut"]
+# for x in fruits:
+#     if x == "banana":
+#         continue
+#     print(x)
+
+# loop range function:
+# for x in range(10):
+#     print(x)
+
+# for x in range(2, 40, 4):
+#     print(x)
+
+# for x in range(6):
+#     print(x)
+# else:
+#     print("Finally finished")
+
+# Nested loops:
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+
