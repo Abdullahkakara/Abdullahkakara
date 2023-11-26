41f91fd6f161bbdd4bd381ed3e0136cdfe5d53cc
Abdullahkakara <mamunlotani@gmail.com>
2023-11-26 14:07:18 +0600

    added the ascii.py

--- ascii.py
+++ ascii.py
@@ -1,26 +1,15 @@
 
-# name = "abdullah"
-
-# for i in range(len(name)):
-#     print(name[i])
-
-# character = 0
-# for i in range(len(name)):
-#     character = character + ord(name[i])
-#     sum = 0
-    
-# print(character)
-
-
-num = 98
-print(chr(num))
-
-letter = input("enter any letter:")
-convert = ord(letter)
-print(convert)
-
 name = "abdullah"
-for i in (len(name))
-sum = 0 
-print(sum(ord(name)))
+
+for i in range(len(name)):
+    print(ord(name[i]))
+
+character = 0
+for i in range(len(name)):
+    character = character + ord(name[i])
+    
+print(character)
+
+
+
 
